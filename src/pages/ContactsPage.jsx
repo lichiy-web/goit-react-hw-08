fbc5e095e46fb8e6ffbc52ef87c8734c62e646a6
Yevhen Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-03-02 13:11:50 +0200

    refactor of pages

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,17 +1,12 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { selectError, selectLoading } from '../redux/contacts/selectors';
+import { useDispatch } from 'react-redux';
 import SearchBox from '../components/SearchBox/SearchBox';
 import ContactList from '../components/ContactList/ContactList';
-import Loader from '../components/Loader/Loader';
-import ErrorMessage from '../components/ErrorMessage/ErrorMessage';
 import ContactForm from '../components/ContactForm/ContactForm';
 import { fetchContacts } from '../redux/contacts/operations';
 import { useEffect } from 'react';
 
 const ContactsPage = () => {
   const dispatch = useDispatch();
-  const loading = useSelector(selectLoading);
-  const error = useSelector(selectError);
 
   useEffect(() => {
     const abortController = new AbortController();
@@ -20,16 +15,9 @@ const ContactsPage = () => {
   }, [dispatch]);
   return (
     <>
-      {error ? (
-        <ErrorMessage />
-      ) : (
-        <>
-          <ContactForm />
-          <SearchBox />
-          <ContactList />
-          <Loader isLoading={loading} />
-        </>
-      )}
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
     </>
   );
 };
